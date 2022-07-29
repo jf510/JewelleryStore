06a529aeeb6edddccf1f76085f020ece60420c88
Jordan <jfontaine19@gmail.com>
2022-07-29 17:10:26 -0400

    imported client form lib. Added key for the top banner. Added the key for the footer banner. exported the serversideprops function.

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,38 @@
 import React from 'react';
-import { Product, FooterBanner, HeroBanner } from '../components'
+import { Product, FooterBanner, HeroBanner } from '../components';
+import { client } from '../lib/client';
 
-const Home = () => {
+const Home = ({ products, bannerData}) => {
   return (
     <div>
-      <HeroBanner />
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
       <div className='products-heading'>
-
         <h2>Best Selling Products</h2>
         <p>Hand-made jewllery of many varitions</p>
       </div>
 
       <div className='products-container'>
-        {['product1', 'product2'].map((product) => product)}
+        {products?.map((product) => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
 
-      <FooterBanner />
+      <FooterBanner footerBanner={ bannerData && bannerData[0]} />
     </div>
-  )
-}
+  );
+  };
 
-export default Home
+  export const getServerSideProps = async () => {
+    const query = '*[_type == "product"]';
+    const products = await client.fetch(query)
+
+    const bannerQuery = '*[_type == "banner"]';
+    const bannerData = await client.fetch(bannerQuery)
+
+    return {
+      props: { products, bannerData }
+    }
+  }
+
+
+export default Home;
