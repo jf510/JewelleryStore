b63a7ee19dc0b1434f53764dd7831b5b85c14862
Jordan <jfontaine19@gmail.com>
2022-07-30 12:42:57 -0400

    created navbar that displays the name, carticon and cart qty

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>Reindorp Jewelery</Link>
+      </p>
+      <button type='button' onClick='' className='cart-icon'>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 
