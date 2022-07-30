3ca13cfec2d191602ced5913fcb37ccc0a4ab0b8
Jordan <jfontaine19@gmail.com>
2022-07-30 12:41:48 -0400

    created html head within the component that wrapps our app.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-function Layout() {
-  return (
-    <div>Layout</div>
+function Layout({ children}) {
+  return ( 
+    <div className='layout'>
+      <Head>
+        <title>Reindorp Jewels</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className='main-container'>
+        {children}
+      </main>
+      <Footer />
+
+    </div>
   )
 }
 
